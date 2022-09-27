bd3c22c2d632363e4d79ae83c90bd983248ab2ab
petterkoding <69626923+petterkoding@users.noreply.github.com>
2022-09-27 12:23:57 +0200

    updated footer content

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -2,9 +2,10 @@
 const Footer = () => {
   return (
     <footer className="w-full h-auto flex flex-col justify-center mt-14 py-8">
-        <div className="h-full flex flex-col items-center">
-            <span className="text-white text-xs capitalize">Halo content owned by Microsoft and developed by 343i</span>
-            <span className="text-white text-xs">| Site and UI by @Petter</span>
+        <div className="h-full flex flex-col items-center w-auto">
+            <span className="text-white text-xs capitalize">Halo content owned by Microsoft | 343i</span>
+            <span className="text-white text-xs my-2">| Site and UI developed by Petter |</span>
+            <span className="text-white text-xs">API: - Autocode and Halo.api</span>
         </div>
     </footer>
   )
