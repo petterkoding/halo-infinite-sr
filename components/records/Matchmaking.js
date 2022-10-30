e5480d98e20a2c03066620c17ae2208bfd7e34a9
petterkoding <69626923+petterkoding@users.noreply.github.com>
2022-10-30 13:23:07 +0100

    gradient background on match stats

--- components/records/Matchmaking.js
+++ components/records/Matchmaking.js
@@ -12,13 +12,13 @@ const Matchmaking = ({matchesPlayed, wins, losses, draws }) => {
           Matches
       </motion.h4>
 
-      <div className="flex flex-row justify-between border-t border-slate-50/70 pb-2 overflow-hidden">
+      <div className="flex flex-row justify-between border-t border-slate-50/70 overflow-hidden">
 
         <motion.div
           initial={{opacity: 0, y: -15}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 0.8, delay: 0.5}}
-          className="pr-2 border-r border-[#648AB1] ">
+          className="pr-2 border-r border-[#648AB1] bg-gradient-to-r from-blue-800/80 to-cyan-500/30 w-full p-1">
           <span className="capitalize text-white text-sm md:text-lg">played</span>
           <span className="block text-white ml-8 text-xl md:text-3xl">{matchesPlayed}</span>
         </motion.div>
@@ -27,7 +27,7 @@ const Matchmaking = ({matchesPlayed, wins, losses, draws }) => {
           initial={{opacity: 0, y: -15}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 0.85, delay: 0.6}}
-          className="pr-2 border-r border-[#648AB1] ">
+          className="pr-2 border-r border-[#648AB1] bg-gradient-to-r from-cyan-500/30 to-indigo-600/70 w-full p-1">
           <span className="capitalize text-white text-sm md:text-lg">wins</span>
           <span className="block text-white ml-8 text-xl md:text-3xl">{wins}</span>
         </motion.div>
@@ -36,7 +36,7 @@ const Matchmaking = ({matchesPlayed, wins, losses, draws }) => {
           initial={{opacity: 0, y: -15}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 0.9, delay: 0.7}}
-          className="pr-2 border-r border-[#648AB1] ">
+          className="pr-2 border-r border-[#648AB1] bg-gradient-to-r from-indigo-600/70 to-violet-500/40 w-full p-1">
           <span className="capitalize text-white text-sm md:text-lg">losses</span>
           <span className="block text-white ml-8 text-xl md:text-3xl">{losses}</span>
         </motion.div>
@@ -45,7 +45,7 @@ const Matchmaking = ({matchesPlayed, wins, losses, draws }) => {
           initial={{opacity: 0, y: -15}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 0.95, delay: 0.8}}
-          className="pr-2 border-r border-[#648AB1] ">
+          className="pr-2 border-r border-[#648AB1] bg-gradient-to-r from-violet-500/40 to-rose-800/80 w-full p-1">
           <span className="capitalize text-white text-sm md:text-lg">draws</span>
           <span className="block text-white ml-8 text-xl md:text-3xl">{draws}</span>
         </motion.div>
